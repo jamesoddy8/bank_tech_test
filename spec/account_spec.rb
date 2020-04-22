93ed78e050b921982f78b3bf4b81ad3eca7c3c95
James Oddy <jamesoddy8@gmail.com>
2020-04-22 17:53:55 +0100

    refactored account_spec

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,32 +1,30 @@
 require 'account'
 
 describe Account do
-  context 'new account' do
-    describe '#initialize' do
-      it 'should have a balance of zero' do
-        expect(subject.balance).to eq(0)
-      end
+  describe '#initialize' do
+    it 'should have a balance of zero' do
+      expect(subject.balance).to eq(0)
     end
+  end
 
-    describe '#deposit' do
-      subject { described_class.new }
-      it 'can deposit money' do
-        expect { subject.deposit(400) }
-          .to change(subject, :balance).by(400)
-      end
+  describe '#deposit' do
+    subject { described_class.new }
+    it 'can deposit money' do
+      expect { subject.deposit(400) }
+      .to change(subject, :balance).by(400)
     end
+  end
 
-    describe '#withdraw' do
-      it 'can withdraw money' do
-        expect { subject.deposit(1000) }
-        .to change(subject, :balance).by(1000)
-        expect { subject.withdraw(200) }
-          .to change(subject, :balance).by(-200)
-      end
+  describe '#withdraw' do
+    it 'can withdraw money' do
+      expect { subject.deposit(1000) }
+      .to change(subject, :balance).by(1000)
+      expect { subject.withdraw(200) }
+      .to change(subject, :balance).by(-200)
     end
+  end
 
-    it 'creates a new account' do
-      expect(subject).to be_instance_of(described_class)
-    end
+  it 'creates a new account' do
+    expect(subject).to be_instance_of(described_class)
   end
 end
